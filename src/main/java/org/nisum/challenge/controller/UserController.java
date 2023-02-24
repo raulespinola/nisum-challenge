21796ea02e0de60e190eec14cecfb047e511feaf
Raul Espinola <raul.espinola@the-cocktail.com>
2023-02-24 20:10:12 -0300

    Nisum: Issue 7: - Add Integration Tests - Fix Controller Tests

--- src/main/java/org/nisum/challenge/controller/UserController.java
+++ src/main/java/org/nisum/challenge/controller/UserController.java
@@ -3,6 +3,7 @@ package org.nisum.challenge.controller;
 
 import io.swagger.annotations.ApiOperation;
 import org.nisum.challenge.controller.dto.*;
+import org.nisum.challenge.exception.CustomAuthenticateException;
 import org.nisum.challenge.mapper.UserMapper;
 import org.nisum.challenge.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,27 +14,23 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
-
 import javax.validation.Valid;
 
 @RestController
-@CrossOrigin
 public class UserController {
-
 	@Autowired
 	private AuthenticationManager authenticationManager;
-
 	@Autowired
-	private UserService userDetailsService;
+	private UserService userService;
 	@Autowired
 	private UserMapper userMapper;
 
 	@ApiOperation(value = "Login User", produces = "application/json")
 	@GetMapping(value = "/login")
-	public ResponseEntity<UserLoginResponseDto> createAuthenticationToken(@RequestBody UserLoginRequestDto authenticationRequest) throws Exception {
+	public ResponseEntity<UserLoginResponseDto> createAuthenticationToken(@RequestBody UserLoginRequestDto authenticationRequest) throws CustomAuthenticateException {
 		authenticate(authenticationRequest.getUsername(), authenticationRequest.getPassword());
 		return ResponseEntity.ok(userMapper
-				.modelToDto(userDetailsService
+				.modelToDto(userService
 						.loadUser(authenticationRequest.getUsername())));
 	}
 	@ApiOperation(value = "Register User - Email Need to be valid forma and Password need " +
@@ -41,17 +38,17 @@ public class UserController {
 			"1 character, Min 8 characters", produces = "application/json")
 	@PostMapping(value = "/register")
 	public ResponseEntity<UserCreateResponseDto> saveUser(@Valid @RequestBody UserCreateRequestDto userRequest)  {
-		return ResponseEntity.status(HttpStatus.CREATED).body(userMapper.modelToDto(userDetailsService
+		return ResponseEntity.status(HttpStatus.CREATED).body(userMapper.modelToDto(userService
 				.save(userMapper.requestDtoToModel(userRequest))));
 	}
 
-	private void authenticate(String username, String password) throws Exception {
+	private void authenticate(String username, String password) throws CustomAuthenticateException {
 		try {
 			authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
 		} catch (DisabledException e) {
-			throw new Exception("USER_DISABLED", e);
+			throw new CustomAuthenticateException("USER_DISABLED", e);
 		} catch (BadCredentialsException e) {
-			throw new Exception("INVALID_CREDENTIALS", e);
+			throw new CustomAuthenticateException("INVALID_CREDENTIALS", e);
 		}
 	}
 }
